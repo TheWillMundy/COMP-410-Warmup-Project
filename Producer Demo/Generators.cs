f9be48068bdf8b5936ff410cf6f1d2c27281c469
Will Mundy <kday5and75@gmail.com>
2020-01-21 14:53:46 -0600

    Added Generators files

---
+++ Producer Demo/Generators.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducerDemo
+{
+    class Generators : IGenerators
+    {
+        public bool Generate()
+        {
+            Console.WriteLine("Generated!");
+            return true;
+        }
+    }
+}
---
+++ Producer Demo/IGenerators.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ProducerDemo
+{
+    public interface IGenerators
+    {
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/Generate",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json, Method = "POST")]
+        bool Generate();
+    }
+}
